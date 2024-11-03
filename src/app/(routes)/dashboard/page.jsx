067b37def5c73abadad8c67c2c7e09f3c2cb0a7a
Kawanlb <kawandev7@gmail.com>
2024-11-03 02:42:07 -0300

    Added 1 component at dashboard page

--- src/app/(routes)/dashboard/page.jsx
+++ src/app/(routes)/dashboard/page.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
-import UserProfile from './_components/UserProfile';
+'use client'
 
-const Dashboard = () => {
+import React from 'react'
+import DoughnutChart from './relatorios/_components/DoughnutChart';
+import styles from '@/app/(routes)/dashboard/relatorios/_styles/Reports.module.css';
+import InvestmentChart from './investimentos/_components/InvestimentChart';
+function Dashboard() {
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <UserProfile />
+    <div className='p-4'>
+      <h2 className='font-bold text-3xl'>Dashboard</h2>
+  
+      <div className={styles.gastosPorCategoria}>
+        <DoughnutChart />
+      </div>  
     </div>
-  );
-};
+
+  )
+}
 
 export default Dashboard;
