5654dd6f7ffde082d018b1bb25573ea21ac0a504
Kawanlb <kawandev7@gmail.com>
2024-11-05 02:16:42 -0300

    using windowhref to reload page and loading button validation

--- src/app/(auth)/_components/SigninForm.jsx
+++ src/app/(auth)/_components/SigninForm.jsx
@@ -1,76 +1,108 @@
-  "use client";
-  import { useState } from "react";
-  import axiosInstance from "@/api/AxiosInstance";
-  import InputField from "./InputField";
-  import Button from "./Button";
-  import CustomLink from "./Link";
-  import FormContainer from "./FormContainer"; // Importa o FormContainer
+"use client";
+import { useState } from "react";
+import axiosInstance from "@/api/AxiosInstance";
+import InputField from "./InputField";
+import Button from "./Button";
+import CustomLink from "./Link";
+import FormContainer from "./FormContainer";
+import { useRouter } from 'next/navigation';
 
-  export default function SigninForm() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState(null); 
+export default function SigninForm() {
+  const router = useRouter();
 
-    const handleSubmit = async (e) => {
-      e.preventDefault();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
-      try {
-        const response = await axiosInstance.post('/auth', {
-          email,
-          password
-        });
+  const validateEmail = (email) => {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(String(email).toLowerCase());
+  };
 
-        const { token } = response.data;
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError(null);
+    setIsLoading(true); 
 
-        // Armazenar o token em um cookie com atributos apropriados, mas sem HttpOnly
-        document.cookie = `authToken=${token}; path=/; Secure; SameSite=None`;
+    if (!validateEmail(email)) {
+      setError("O email fornecido é inválido");
+      setIsLoading(false);
+      return;
+    }
 
-        console.log("Autenticado com sucesso:", token);
+    try {
+      const response = await axiosInstance.post('/auth', {
+        email,
+        password
+      });
 
-        // Redirecionar ou fazer algo após a autenticação
-        // Por exemplo, redirecionar para a página inicial
-        window.location.href = '/dashboard';
+      const { token } = response.data;
+      document.cookie = `authToken=${token}; path=/; Secure; SameSite=None`;
 
-      } catch (error) {
-        setError("Erro na autenticação. Verifique suas credenciais.");
-        console.error("Erro ao autenticar:", error);
+    window.location.href = '/dashboard' ;
+      setEmail('');
+      setPassword('');
+
+    } catch (error) {
+      if (error.response) {
+        const { status, data } = error.response;
+        let errorMessage = 'Falha ao autenticar';
+
+        if (status === 400) {
+          errorMessage = data.message || 'Credenciais inválidas';
+        } else if (status === 401) {
+          errorMessage = 'Não autorizado. Verifique suas credenciais.';
+        } else if (status === 500) {
+          errorMessage = 'Erro interno do servidor. Tente novamente mais tarde.';
+        }
+
+        setError(errorMessage);
+      } else {
+        setError('Erro desconhecido. Verifique sua conexão com a internet.');
       }
-    };
 
-    return (
-      <FormContainer>
-        <div className="max-w-md mx-auto px-4 py-6">
-          <h1 className="text-3xl font-bold text-gray-900 sm:text-4xl">
-            Bem-vindo de volta
-          </h1>
-          <p className="mt-2 text-sm text-gray-600">
-            Não tem uma conta?{' '}
-            <a href="/sign-up" className="font-medium text-indigo-600 hover:text-indigo-500">
-              Inscreva-se
-            </a>
-          </p>
-          {error && <p className="text-red-500">{error}</p>}
-          <form onSubmit={handleSubmit} className="space-y-6">
-            <InputField
-              label="Email"
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              id="email"
-            />
-            <InputField
-              label="Senha"
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              id="password"
-            />
-            <div className="flex flex-col sm:flex-row items-center justify-between">
-              <Button label="Entrar" />
-              <CustomLink href="/change-password" label="Esqueceu sua senha?" />
-            </div>
-          </form>
-        </div>
-      </FormContainer>
-    );
-  }
+      console.error('Error:', error.response ? error.response.data : error.message);
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <FormContainer>
+      <div className="max-w-md mx-auto px-4 py-6">
+        <h1 className="text-3xl font-bold text-gray-900 sm:text-4xl">
+          Bem-vindo de volta
+        </h1>
+        <p className="mt-2 text-sm text-gray-600">
+          Não tem uma conta?{' '}
+          <a href="/sign-up" className="font-medium text-indigo-600 hover:text-indigo-500">
+            Inscreva-se
+          </a>
+        </p>
+        {error && <p className="text-red-500">{error}</p>}
+        <form onSubmit={handleSubmit} className="space-y-6">
+          <InputField
+            label="Email"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            id="email"
+            disabled={isLoading} 
+          />
+          <InputField
+            label="Senha"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            id="password"
+            disabled={isLoading} 
+          />
+          <div className="flex flex-col sm:flex-row items-center justify-between">
+            <Button label="Entrar" isLoading={isLoading} />
+            <CustomLink href="/change-password" label="Esqueceu sua senha?" />
+          </div>
+        </form>
+      </div>
+    </FormContainer>
+  );
+}
