5e7c14df1273c93e31a503859f4dd09b9b1b4684
kawanlb <kawandev7@gmail.com>
2024-11-19 20:48:54 -0300

    correct css

--- src/app/(routes)/dashboard/layout.jsx
+++ src/app/(routes)/dashboard/layout.jsx
@@ -1,9 +1,9 @@
 'use client';
 
 import React, { useState } from "react";
-import { AuthProvider } from "@/context/authContext"; 
+import { AuthProvider } from "@/context/authContext";
 import Sidebar from "./_components/Sidebar";
-import "../styles/globals.css";
+import "@/styles/global_dashboard.css";
 
 export default function DashboardLayout({ children }) {
   const [isSidebarVisible, setIsSidebarVisible] = useState(true);
@@ -14,10 +14,14 @@ export default function DashboardLayout({ children }) {
 
   return (
     <AuthProvider>
-      <div className="flex h-screen">
+      <div className="dashboard-container">
         <Sidebar onToggle={handleSidebarToggle} />
-        <div className={`flex-1 transition-all duration-300 ease-in-out ${isSidebarVisible ? "ml-48" : "ml-16"} overflow-hidden`}>
-          <main className="p-4 h-full">{children}</main>
+        <div
+          className={`main-content transition-all duration-300 ease-in-out ${
+            isSidebarVisible ? "ml-48" : "ml-16"
+          }`}
+        >
+          {children}
         </div>
       </div>
     </AuthProvider>
